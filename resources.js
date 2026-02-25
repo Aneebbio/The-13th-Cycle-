336b4c039f5bb35aaaed6c32176f28c92c171f0a
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-25 20:45:04 +0500

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,9 @@
+// resources.js
+export const Resources = {
+    memory: { id: 'memory', label: 'Memory', category: 'Soul' },
+    iron: { id: 'iron', label: 'Iron', category: 'Common' },
+    copper: { id: 'copper', label: 'Copper', category: 'Common' },
+    titanium: { id: 'titanium', label: 'Titanium', category: 'Rare' },
+    star_plasma: { id: 'star_plasma', label: 'Star Plasma', category: 'Ancient' },
+    nothingness: { id: 'nothingness', label: 'Nothingness', category: 'Void' }
+};
