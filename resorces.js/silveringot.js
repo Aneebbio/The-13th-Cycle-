26a83a067b8eaaac8440213b1d6c5b15be1d888b
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:34:26 +0500

    Create silveringot.js

---
+++ resorces.js/silveringot.js
@@ -0,0 +1,7 @@
+export const silveringot = {
+    id: "silveringot",
+    name: "Silver Ingot",
+    type: "processed",
+    requires: { id: "silverore", qty: 3 },
+    desc: "Refined silver with the highest thermal and electrical conductivity."
+};
