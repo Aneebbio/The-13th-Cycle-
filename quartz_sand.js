d505dcb61acab19f66fb4b3134fb1660ce8aa56c
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:14:18 +0500

    Delete quartz_sand.js

--- quartz_sand.js
+++
@@ -1,7 +0,0 @@
-export const quartz_sand = {
-    id: "quartz_sand",
-    name: "Quartz Sand",
-    type: "ore",
-    origin: "earth",
-    desc: "High-purity silica sand found in Earth's riverbeds and deserts."
-};
