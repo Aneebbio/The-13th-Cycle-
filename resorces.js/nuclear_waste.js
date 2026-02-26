d65d2181b79c13d6b0b843bdf5d9d762f8931979
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:25:05 +0500

    Create nuclear_waste.js

---
+++ resorces.js/nuclear_waste.js
@@ -0,0 +1,7 @@
+export const nuclear_waste = {
+    id: "nuclear_waste",
+    label: "Nuclear Waste",
+    type: "waste",
+    hazard: "high",
+    description: "Highly radioactive byproduct. Requires lead containment."
+};
