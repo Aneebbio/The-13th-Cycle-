c215c5162e8f404d400a0061dee8343f6bace825
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:29:18 +0500

    Create purified_water.js

---
+++ resorces.js/purified_water.js
@@ -0,0 +1,7 @@
+export const purified_water = {
+    id: "purified_water",
+    label: "Purified Water",
+    type: "refined_fluid",
+    requires: { item: "raw_water", quantity: 1 },
+    desc: "Clean, H2O. Essential for human life and cooling reactors."
+};
