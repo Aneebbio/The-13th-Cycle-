8067ea3ef63f885df585a4cb6aeccde049c44b2d
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:35:06 +0500

    Create amethyst.js

---
+++ resorces.js/amethyst.js
@@ -0,0 +1,7 @@
+export const amethyst = {
+    id: "amethyst",
+    name: "Amethyst",
+    type: "gem",
+    requires: { id: "deeprock", qty: 5 },
+    desc: "A rare purple crystal extracted from the pressure of deep rock."
+};
