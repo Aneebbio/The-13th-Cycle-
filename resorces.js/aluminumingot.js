b15135e271e6bf46d96fcabf6dd50614da34ba54
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:49:59 +0500

    Create aluminumingot.js

---
+++ resorces.js/aluminumingot.js
@@ -0,0 +1,7 @@
+export const aluminumingot = {
+    id: "aluminumingot",
+    name: "Aluminum Ingot",
+    type: "ingot",
+    requires: { id: "aluminumore", qty: 3 },
+    desc: "Lightweight and corrosion-resistant refined metal."
+};
