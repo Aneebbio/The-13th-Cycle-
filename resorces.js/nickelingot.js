1e64e7ad8e3e89c3002f59686e54d18d34acb61e
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:56:13 +0500

    Create nickelingot.js

---
+++ resorces.js/nickelingot.js
@@ -0,0 +1,7 @@
+export const nickelingot = {
+    id: "nickelingot",
+    name: "Nickel Ingot",
+    type: "ingot",
+    requires: { id: "nickelore", qty: 2 },
+    desc: "Resistant to oxidation; used in high-temp equipment."
+};
