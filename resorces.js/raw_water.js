6b8bc6dd8d47adf52bb9a2c11ca3e04f3e81c2cc
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:28:55 +0500

    Create raw_water.js

---
+++ resorces.js/raw_water.js
@@ -0,0 +1,7 @@
+export const raw_water = {
+    id: "raw_water",
+    label: "Raw Water",
+    type: "raw_fluid",
+    origin: "earth_lakes",
+    desc: "Unfiltered water. Contains bacteria and minerals. Unsafe to drink."
+};
