eaa7ebb727086a39354dbdfe32da871d3680c4f6
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:36:44 +0500

    Create liquid_nitrogen.js

---
+++ resorces.js/liquid_nitrogen.js
@@ -0,0 +1,6 @@
+export const liquid_nitrogen = {
+    id: "liquid_nitrogen",
+    label: "Liquid Nitrogen",
+    type: "fluid",
+    desc: "Used for cryogenic cooling of super-computers and reactor cores."
+};
