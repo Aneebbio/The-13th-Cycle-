4a46f2a3f25cc0c254778a704ff21df8a9d0440f
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:37:16 +0500

    Create sulfuric_acid.js

---
+++ resorces.js/sulfuric_acid.js
@@ -0,0 +1,7 @@
+export const sulfuric_acid = {
+    id: "sulfuric_acid",
+    label: "Sulfuric Acid",
+    type: "acid",
+    requires: { item: "sulfur_powder", quantity: 2 },
+    desc: "A corrosive fluid used to dissolve rock and isolate precious metals."
+};
