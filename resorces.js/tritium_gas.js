a6b1a0846bf63dfdea94b7656cc880085aaa458a
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:34:46 +0500

    Create tritium_gas.js

---
+++ resorces.js/tritium_gas.js
@@ -0,0 +1,6 @@
+export const tritium_gas = {
+    id: "tritium_gas",
+    label: "Tritium Gas",
+    type: "waste_output",
+    desc: "A radioactive isotope of hydrogen; a rare byproduct of heavy water reactors."
+};
