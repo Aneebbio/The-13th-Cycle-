0963a372608552efe8009b7564651819f8be725c
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:32:56 +0500

    Create martian_water_extract.js

---
+++ resorces.js/martian_water_extract.js
@@ -0,0 +1,7 @@
+export const martian_water_extract = {
+    id: "martian_water_extract",
+    label: "Martian Water Extract",
+    type: "refined",
+    requires: { item: "martian_permafrost", quantity: 3 },
+    desc: "Water extracted from Martian soil via thermal evaporation."
+};
