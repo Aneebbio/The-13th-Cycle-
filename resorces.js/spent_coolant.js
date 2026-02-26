d59ad6555ff327897f60b8091a3b0507f09b5edd
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:38:53 +0500

    Create spent_coolant.js

---
+++ resorces.js/spent_coolant.js
@@ -0,0 +1,6 @@
+export const spent_coolant = {
+    id: "spent_coolant",
+    label: "Spent Coolant",
+    type: "fluid_waste",
+    desc: "Warmed-up nitrogen or water that can no longer cool the machines."
+};
