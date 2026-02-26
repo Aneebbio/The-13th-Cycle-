693769b902a0c23f1b7b5a41826a39b09335d011
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:50:13 +0500

    Create sulfur_dioxide.js

---
+++ resorces.js/sulfur_dioxide.js
@@ -0,0 +1,6 @@
+export const sulfur_dioxide = {
+    id: "sulfur_dioxide",
+    label: "Sulfur Dioxide",
+    type: "toxic_gas",
+    desc: "A sharp-smelling byproduct of smelting sulfur-rich ores. Causes acid rain in bio-domes."
+};
