3e6d2bf680455c68e74ffb625ab3671fb5639be2
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:50:54 +0500

    Create ammonia_gas.js

---
+++ resorces.js/ammonia_gas.js
@@ -0,0 +1,7 @@
+export const ammonia_gas = {
+    id: "ammonia_gas",
+    label: "Ammonia Gas",
+    type: "chemical_gas",
+    requires: { item: "nitrogen_gas", quantity: 1, item2: "hydrogen_gas", quantity2: 3 },
+    desc: "Essential for producing high-grade fertilizers and station refrigerants."
+};
