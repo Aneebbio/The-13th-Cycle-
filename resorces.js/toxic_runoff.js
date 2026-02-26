55e834d5fda48dd79257e559e2db2192707c0ce4
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:38:23 +0500

    Create toxic_runoff.js

---
+++ resorces.js/toxic_runoff.js
@@ -0,0 +1,6 @@
+export const toxic_runoff = {
+    id: "toxic_runoff",
+    label: "Toxic Runoff",
+    type: "fluid_waste",
+    desc: "Contaminated liquid from the metal leaching process. Deadly if leaked."
+};
