df4338f173e543b6db8dc75eca5905c4ec1e73fb
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:46:33 +0500

    Create martian_air_raw.js

---
+++ resorces.js/martian_air_raw.js
@@ -0,0 +1,6 @@
+export const martian_air_raw = {
+    id: "martian_air_raw",
+    label: "Raw Martian Air",
+    type: "environmental_gas",
+    desc: "95% Carbon Dioxide. Unbreathable without heavy processing."
+};
