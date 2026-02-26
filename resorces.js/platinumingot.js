976a1827313a7fbbc12fc8872a7f22c3a6c6fcd3
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:52:39 +0500

    Create platinumingot.js

---
+++ resorces.js/platinumingot.js
@@ -0,0 +1,7 @@
+export const platinumingot = {
+    id: "platinumingot",
+    name: "Platinum Ingot",
+    type: "ingot",
+    requires: { id: "platinumore", qty: 4 },
+    desc: "A high-value, catalytic precious metal bar."
+};
