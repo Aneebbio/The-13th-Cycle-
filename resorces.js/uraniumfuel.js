9751ef4c05334933c8291c5c06499bf608a81acb
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:41:28 +0500

    Create uraniumfuel.js

---
+++ resorces.js/uraniumfuel.js
@@ -0,0 +1,7 @@
+export const uraniumfuel = {
+    id: "uraniumfuel",
+    name: "Enriched Uranium",
+    type: "processed",
+    requires: { id: "uraniumore", qty: 5 },
+    description: "Concentrated energy source for warp drives and reactors."
+};
