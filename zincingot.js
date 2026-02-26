102a789f05a51eba785a651f66beab97ce63d155
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:12:43 +0500

    Delete zincingot.js

--- zincingot.js
+++
@@ -1,7 +0,0 @@
-export const zincingot = {
-    id: "zincingot",
-    name: "Zinc Ingot",
-    type: "ingot",
-    requires: { id: "zincore", qty: 2 },
-    desc: "Refined zinc bar, essential for alloys like brass."
-};
