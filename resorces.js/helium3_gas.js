cba86084a7ac0423fd9f49c57fa3f159b20d81a0
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:49:46 +0500

    Create helium3_gas.js

---
+++ resorces.js/helium3_gas.js
@@ -0,0 +1,7 @@
+export const helium3_gas = {
+    id: "helium3_gas",
+    label: "Helium-3",
+    type: "rare_gas",
+    origin: "moon",
+    desc: "Harvested from Lunar Regolith. The cleanest fuel for fusion power."
+};
