d0eca7010dc379ed73020a6af0848d2f5c2f0163
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:40:40 +0500

    Create titaniumore.js

---
+++ resorces.js/titaniumore.js
@@ -0,0 +1,7 @@
+export const titaniumore = {
+    id: "titaniumore",
+    name: "Titanium Ore",
+    type: "ore",
+    rarity: "rare",
+    description: "Extremely strong and lightweight. Found in rutile deposits."
+};
