eab9f76bfb923a5bcf6d3c0a1188d224431165b9
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:05:19 +0500

    Create martian_regolith.js

---
+++ resorces.js/martian_regolith.js
@@ -0,0 +1,6 @@
+export const martian_regolith = {
+    id: "martian_regolith",
+    name: "Martian Regolith",
+    type: "raw",
+    desc: "Iron-oxide rich dust from Mars. The reason for the red color."
+};
