5a26b81b8ff452bae8e5d05ae2bf7845bc2abd14
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:14:44 +0500

    Create quartz_sand.js

---
+++ resorces.js/quartz_sand.js
@@ -0,0 +1,7 @@
+export const quartz_sand = {
+    id: "quartz_sand",
+    name: "Quartz Sand",
+    type: "ore",
+    origin: "earth",
+    desc: "High-purity silica sand found in Earth's riverbeds and deserts."
+};
