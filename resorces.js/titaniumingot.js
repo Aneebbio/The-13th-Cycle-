274dbca9b034b2472633db83c57fa32133a6aa76
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:42:00 +0500

    Create titaniumingot.js

---
+++ resorces.js/titaniumingot.js
@@ -0,0 +1,7 @@
+export const titaniumingot = {
+    id: "titaniumingot",
+    name: "Titanium Ingot",
+    type: "processed",
+    requires: { id: "titaniumore", qty: 3 },
+    description: "An aerospace-grade metal bar, resistant to cosmic corrosion."
+};
