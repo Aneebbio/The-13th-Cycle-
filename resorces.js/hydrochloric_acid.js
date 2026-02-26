85aebd25296d6ebe9862e48617b213848a3bb0d7
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:37:38 +0500

    Create hydrochloric_acid.js

---
+++ resorces.js/hydrochloric_acid.js
@@ -0,0 +1,7 @@
+export const hydrochloric_acid = {
+    id: "hydrochloric_acid",
+    label: "Hydrochloric Acid",
+    type: "acid",
+    requires: { item: "table_salt", quantity: 2 },
+    desc: "Strong acid used in heavy industrial cleaning and refining."
+};
