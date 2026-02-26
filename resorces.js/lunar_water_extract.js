f3496cb0cd08737a75cc51733783ced7c07044b7
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:31:58 +0500

    Create lunar_water_extract.js

---
+++ resorces.js/lunar_water_extract.js
@@ -0,0 +1,7 @@
+export const lunar_water_extract = {
+    id: "lunar_water_extract",
+    label: "Lunar Water Extract",
+    type: "refined",
+    requires: { item: "lunar_ice", quantity: 2 },
+    desc: "Melted and filtered lunar ice, ready for use or electrolysis."
+};
