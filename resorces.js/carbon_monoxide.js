7325032238348fa64cd2e653c5c0d13a9788075f
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:50:33 +0500

    Create carbon_monoxide.js

---
+++ resorces.js/carbon_monoxide.js
@@ -0,0 +1,6 @@
+export const carbon_monoxide = {
+    id: "carbon_monoxide",
+    label: "Carbon Monoxide",
+    type: "toxic_gas",
+    desc: "An odorless, deadly byproduct of incomplete fuel combustion. Requires sensors."
+};
