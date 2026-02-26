1114a05a3f139ca8c78e9ed8010ae8dfda1a7905
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:16:30 +0500

    Create sulfur_chunk.js

---
+++ resorces.js/sulfur_chunk.js
@@ -0,0 +1,7 @@
+export const sulfur_chunk = {
+    id: "sulfur_chunk",
+    name: "Sulfur Chunk",
+    type: "ore",
+    origin: "earth",
+    desc: "Bright yellow crystalline sulfur collected from volcanic fumaroles."
+};
