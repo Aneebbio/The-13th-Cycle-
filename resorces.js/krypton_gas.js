f735ec7dcfa1b5cdb7fca4aeab09491f69b8e022
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:47:54 +0500

    Create krypton_gas.js

---
+++ resorces.js/krypton_gas.js
@@ -0,0 +1,6 @@
+export const krypton_gas = {
+    id: "krypton_gas",
+    label: "Krypton Gas",
+    type: "noble_gas",
+    desc: "Used in high-powered gas lasers and advanced station lighting."
+};
