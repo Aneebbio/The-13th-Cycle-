e7e179c27921bea30623bb4088bd54d814756776
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:52:12 +0500

    Create copper_wire.js

---
+++ resorces.js/copper_wire.js
@@ -0,0 +1,7 @@
+export const copper_wire = {
+    id: "copper_wire",
+    label: "Copper Wire",
+    type: "component",
+    requires: { item: "copperingot", quantity: 1 },
+    desc: "Standard conductive threading for power distribution."
+};
