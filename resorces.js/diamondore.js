f1f8a0a59b9b042b2ad44cd3b22b1c158f8adf31
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:41:05 +0500

    Create diamondore.js

---
+++ resorces.js/diamondore.js
@@ -0,0 +1,7 @@
+export const diamondore = {
+    id: "diamondore",
+    name: "Diamond Ore",
+    type: "ore",
+    rarity: "epic",
+    description: "Pure carbon compressed over billions of years. Hardest natural material."
+};
