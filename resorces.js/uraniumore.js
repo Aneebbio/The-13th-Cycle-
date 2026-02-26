ed5b6a8c7f1d72a138dcf72ac5040511eb991b6f
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:40:17 +0500

    Create uraniumore.js

---
+++ resorces.js/uraniumore.js
@@ -0,0 +1,7 @@
+export const uraniumore = {
+    id: "uraniumore",
+    name: "Uranium Ore",
+    type: "ore",
+    rarity: "legendary",
+    description: "A heavy, radioactive mineral. Pulses with a faint green glow."
+};
