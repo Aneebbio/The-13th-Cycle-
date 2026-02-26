afc628b4b86d9ecd5ef1c3d7620346e7e68621a7
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:47:32 +0500

    Create xenon_gas.js

---
+++ resorces.js/xenon_gas.js
@@ -0,0 +1,6 @@
+export const xenon_gas = {
+    id: "xenon_gas",
+    label: "Xenon Gas",
+    type: "noble_gas",
+    desc: "A heavy noble gas. The perfect propellant for high-efficiency Ion Thrusters."
+};
