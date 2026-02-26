a0f242b0fa8f52e869185993df476a10a1d8bd7e
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:16:05 +0500

    Create silicon_ingot.js

---
+++ resorces.js/silicon_ingot.js
@@ -0,0 +1,7 @@
+export const silicon_ingot = {
+    id: "silicon_ingot",
+    name: "Silicon Ingot",
+    type: "ingot",
+    requires: { id: "quartz_sand", qty: 4 },
+    desc: "A purified metallurgical-grade silicon block."
+};
