a5a278b8242267d3b439f5c22328a9e8fb976a75
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:22:24 +0500

    Create petrol.js

---
+++ resorces.js/petrol.js
@@ -0,0 +1,11 @@
+export const petrol = {
+    id: 'petrol',
+    label: 'Petrol',
+    type: 'refined',
+    requires: {
+        item: 'oil',
+        quantity: 2
+    },
+    value: 45,
+    description: 'Refined liquid fuel for high-combustion engines.'
+};
