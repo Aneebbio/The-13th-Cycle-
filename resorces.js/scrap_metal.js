bc84c792dc9004322082b93870da192fb240b65d
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:25:54 +0500

    Create scrap_metal.js

---
+++ resorces.js/scrap_metal.js
@@ -0,0 +1,6 @@
+export const scrap_metal = {
+    id: "scrap_metal",
+    label: "Scrap Metal",
+    type: "waste",
+    description: "Twisted bits of iron and copper from failed machine cycles."
+};
