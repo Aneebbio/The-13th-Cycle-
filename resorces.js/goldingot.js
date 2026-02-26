2549ed80b0441696cfddded4f3f2ca0012723c09
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:33:41 +0500

    Create goldingot.js

---
+++ resorces.js/goldingot.js
@@ -0,0 +1,7 @@
+export const goldingot = {
+    id: "goldingot",
+    name: "Gold Ingot",
+    type: "processed",
+    requires: { id: "goldore", qty: 3 },
+    desc: "Highly valuable and conductive; used in advanced space tech."
+};
