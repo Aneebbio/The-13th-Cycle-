8e6dc4e08347e80724fc4549a73a39c21aa0e2df
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:30:05 +0500

    Create heavy_water.js

---
+++ resorces.js/heavy_water.js
@@ -0,0 +1,7 @@
+export const heavy_water = {
+    id: "heavy_water",
+    label: "Heavy Water (Deuterium)",
+    type: "reactor_output",
+    requires: { item: "purified_water", quantity: 10 },
+    desc: "Water with a high hydrogen isotope count. Used in nuclear fusion."
+};
