4e0d3de1e16e4bfb637ad126730c9b580ba4d5d3
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:48:17 +0500

    Create deuterium_gas.js

---
+++ resorces.js/deuterium_gas.js
@@ -0,0 +1,7 @@
+export const deuterium_gas = {
+    id: "deuterium_gas",
+    label: "Deuterium Gas",
+    type: "fusion_fuel",
+    requires: { item: "heavy_water", quantity: 5 },
+    desc: "Heavy hydrogen used as a primary fuel for stable fusion reactors."
+};
