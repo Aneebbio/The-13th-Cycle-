c6bb059526e3129716dc9085f048f12ba5a9eb40
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:25:26 +0500

    Create depleted_uranium.js

---
+++ resorces.js/depleted_uranium.js
@@ -0,0 +1,7 @@
+export const depleted_uranium = {
+    id: "depleted_uranium",
+    label: "Depleted Uranium",
+    type: "material",
+    requires: { item: "nuclear_waste", quantity: 5 },
+    description: "Heavy metal byproduct used for armor plating."
+};
