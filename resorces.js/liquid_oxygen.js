e9153d4055c0afc7c28589d222bb8204054fe2e4
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:35:53 +0500

    Create liquid_oxygen.js

---
+++ resorces.js/liquid_oxygen.js
@@ -0,0 +1,7 @@
+export const liquid_oxygen = {
+    id: "liquid_oxygen",
+    label: "Liquid Oxygen (LOX)",
+    type: "fluid",
+    requires: { item: "purified_water", quantity: 5 }, // Via Electrolysis
+    desc: "Highly concentrated oxidizer for rocket engines and medical life support."
+};
