d6f1d1796c4b69f71dfd36eff4cc732853377677
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:57:21 +0500

    Create tungsteningot.js

---
+++ resorces.js/tungsteningot.js
@@ -0,0 +1,7 @@
+export const tungsteningot = {
+    id: "tungsteningot",
+    name: "Tungsten Ingot",
+    type: "ingot",
+    requires: { id: "tungstenore", qty: 4 },
+    desc: "The hardest refined metal bar in the station's inventory."
+};
