5893c28505a5e02786fbbdf41aa206a7a5274858
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:26:53 +0500

    Create toxic_sludge.js

---
+++ resorces.js/toxic_sludge.js
@@ -0,0 +1,6 @@
+export const toxic_sludge = {
+    id: "toxic_sludge",
+    label: "Toxic Sludge",
+    type: "fluid_waste",
+    description: "Chemical runoff from heavy oil refining and fluid processing."
+};
