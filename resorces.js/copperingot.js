283d23f122f3ee6704173203333613cbd37ba4bc
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:12:18 +0500

    Create copperingot.js

---
+++ resorces.js/copperingot.js
@@ -0,0 +1,7 @@
+export const copperingot = {
+    id: "copperingot",
+    name: "Copper Ingot",
+    type: "processed",
+    requires: { id: "copperore", qty: 2 },
+    desc: "Refined copper, essential for conductive wiring and electronics."
+};
