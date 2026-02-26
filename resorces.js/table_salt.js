5769a4fd4e26b6ddb28bf94af8f942b7e65ba430
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:18:44 +0500

    Create table_salt.js

---
+++ resorces.js/table_salt.js
@@ -0,0 +1,7 @@
+export const table_salt = {
+    id: "table_salt",
+    name: "Refined Salt",
+    type: "processed",
+    requires: { id: "halite", qty: 1 },
+    desc: "Purified sodium chloride."
+};
