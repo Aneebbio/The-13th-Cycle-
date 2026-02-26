02ff8ac7979085eb51dd2bb840b179ab892b5288
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:52:39 +0500

    Create gold_wire.js

---
+++ resorces.js/gold_wire.js
@@ -0,0 +1,7 @@
+export const gold_wire = {
+    id: "gold_wire",
+    label: "Gold Wire",
+    type: "component",
+    requires: { item: "goldingot", quantity: 1 },
+    desc: "Highly conductive, corrosion-resistant wire for high-end electronics."
+};
