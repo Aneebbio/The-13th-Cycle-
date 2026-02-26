2add198b122fec74305e4fd28cac93657e1a2322
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:32:35 +0500

    Create tiningot.js

---
+++ resorces.js/tiningot.js
@@ -0,0 +1,7 @@
+export const tiningot = {
+    id: "tiningot",
+    name: "Tin Ingot",
+    type: "processed",
+    requires: { id: "tinore", qty: 2 },
+    desc: "A soft, corrosion-resistant metal used for soldering."
+};
