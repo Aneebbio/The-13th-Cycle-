364dedf9a5b6bb1f83e401a619f4059fd3a37dde
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:00:58 +0500

    Create charcoal.js

---
+++ resorces.js/charcoal.js
@@ -0,0 +1,7 @@
+export const charcoal = {
+    id: "charcoal",
+    name: "Charcoal",
+    type: "processed_fuel",
+    requires: { id: "wood", qty: 2 },
+    desc: "Lightweight carbon fuel created by heating wood."
+};
