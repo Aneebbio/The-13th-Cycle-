65d9392c78538dd8270215d970c9ec40cdafaf03
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:17:44 +0500

    Create sulfur_powder.js

---
+++ resorces.js/sulfur_powder.js
@@ -0,0 +1,7 @@
+export const sulfur_powder = {
+    id: "sulfur_powder",
+    name: "Refined Sulfur Powder",
+    type: "processed",
+    requires: { id: "sulfur_chunk", qty: 1 },
+    desc: "Fine-ground sulfur used in chemical manufacturing and gunpowder."
+};
