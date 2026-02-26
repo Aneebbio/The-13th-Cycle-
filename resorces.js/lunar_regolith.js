f8ae9a5d188603189593b910381c89bc98cb2191
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:04:36 +0500

    Create lunar_regolith.js

---
+++ resorces.js/lunar_regolith.js
@@ -0,0 +1,6 @@
+export const lunar_regolith = {
+    id: "lunar_regolith",
+    name: "Lunar Regolith",
+    type: "raw",
+    desc: "Sharp, glass-like dust from the Moon's surface. Rich in Helium-3."
+};
