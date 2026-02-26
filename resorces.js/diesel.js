1512e18baca5faa4ae742a4ba2d4a759160301c2
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:22:48 +0500

    Create diesel.js

---
+++ resorces.js/diesel.js
@@ -0,0 +1,11 @@
+export const diesel = {
+    id: 'diesel',
+    label: 'Diesel',
+    type: 'refined',
+    requires: {
+        item: 'oil',
+        quantity: 3
+    },
+    value: 60,
+    description: 'Heavy industrial fuel for generators.'
+};
