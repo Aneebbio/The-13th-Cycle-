26b33a37aac55ae31a372a7dd0caca8ab64bc082
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:33:02 +0500

    Create leadingot.js

---
+++ resorces.js/leadingot.js
@@ -0,0 +1,7 @@
+export const leadingot = {
+    id: "leadingot",
+    name: "Lead Ingot",
+    type: "processed",
+    requires: { id: "leadore", qty: 2 },
+    desc: "Densely packed lead, used for radiation shielding and batteries."
+};
