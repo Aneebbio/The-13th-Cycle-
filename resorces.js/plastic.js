0694d29d4b65f665d002cc7a60dd08109880e616
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:23:15 +0500

    Create plastic.js

---
+++ resorces.js/plastic.js
@@ -0,0 +1,11 @@
+export const plastic = {
+    id: 'plastic',
+    label: 'Plastic',
+    type: 'refined',
+    requires: {
+        item: 'oil',
+        quantity: 1
+    },
+    value: 25,
+    description: 'Synthetic material produced from petroleum by-products.'
+};
