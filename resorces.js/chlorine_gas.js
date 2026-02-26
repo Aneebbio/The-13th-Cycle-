1265f7645c99548fb87feb4db88771301c11ac50
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:45:49 +0500

    Create chlorine_gas.js

---
+++ resorces.js/chlorine_gas.js
@@ -0,0 +1,6 @@
+export const chlorine_gas = {
+    id: "chlorine_gas",
+    label: "Chlorine Gas",
+    type: "toxic_gas",
+    desc: "A greenish-yellow toxic gas byproduct of salt electrolysis."
+};
