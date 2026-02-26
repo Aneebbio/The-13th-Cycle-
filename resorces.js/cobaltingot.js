14ffabfbd4513832cb2e15c1efc48bcb3089a511
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:53:37 +0500

    Create cobaltingot.js

---
+++ resorces.js/cobaltingot.js
@@ -0,0 +1,7 @@
+export const cobaltingot = {
+    id: "cobaltingot",
+    name: "Cobalt Ingot",
+    type: "ingot",
+    requires: { id: "cobaltore", qty: 3 },
+    desc: "A hard, lustrous blue-grey metal bar."
+};
