07afafb8d66bec69c21ef62a8bbaf7021f82549a
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:42:32 +0500

    Create carbon_dioxide.js

---
+++ resorces.js/carbon_dioxide.js
@@ -0,0 +1,6 @@
+export const carbon_dioxide = {
+    id: "carbon_dioxide",
+    label: "Carbon Dioxide",
+    type: "gas",
+    desc: "A byproduct of breathing and fuel combustion. Required for plants."
+};
