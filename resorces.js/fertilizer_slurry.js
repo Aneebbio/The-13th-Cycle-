f5f4191dcf3e53c8f2327dc4a942d61a5dd5041f
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:39:26 +0500

    Create fertilizer_slurry.js

---
+++ resorces.js/fertilizer_slurry.js
@@ -0,0 +1,7 @@
+export const fertilizer_slurry = {
+    id: "fertilizer_slurry",
+    label: "Fertilizer Slurry",
+    type: "bio_fluid",
+    requires: { item: "waste_water", quantity: 5 },
+    desc: "Nutrient-rich liquid used to accelerate plant growth in hydroponics."
+};
