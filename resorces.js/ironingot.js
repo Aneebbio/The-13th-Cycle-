9887e0e42dfc1f23961fc0146ef8bf7c33f52e7a
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 19:31:08 +0500

    Create ironingot.js

---
+++ resorces.js/ironingot.js
@@ -0,0 +1,7 @@
+export const ironingot = {
+    id: "ironingot",
+    name: "Iron Ingot",
+    type: "processed",
+    requires: { id: "ironore", qty: 2 },
+    desc: "A solid bar of refined iron, the backbone of industry."
+};
