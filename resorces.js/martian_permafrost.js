66104653010028d007e0d39590244206b9250dbd
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:32:20 +0500

    Create martian_permafrost.js

---
+++ resorces.js/martian_permafrost.js
@@ -0,0 +1,7 @@
+export const martian_permafrost = {
+    id: "martian_permafrost",
+    label: "Martian Permafrost",
+    type: "raw",
+    origin: "mars",
+    desc: "Iron-rich soil frozen with water content."
+};
