f8437824f7af72f95663bd83b9692b6d05d85ed0
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:36:20 +0500

    Create liquid_hydrogen.js

---
+++ resorces.js/liquid_hydrogen.js
@@ -0,0 +1,7 @@
+export const liquid_hydrogen = {
+    id: "liquid_hydrogen",
+    label: "Liquid Hydrogen",
+    type: "fluid",
+    requires: { item: "purified_water", quantity: 5 },
+    desc: "The primary fuel for deep-space thermal rockets."
+};
