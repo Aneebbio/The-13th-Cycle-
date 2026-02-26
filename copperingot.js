4a574494d23ecf0247dda197423c26c546fa1e7a
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-26 20:11:09 +0500

    Delete copperingot.js

--- copperingot.js
+++
@@ -1,7 +0,0 @@
-export const copperingot = {
-    id: "copperingot",
-    name: "Copper Ingot",
-    type: "processed",
-    requires: { id: "copperore", qty: 2 },
-    desc: "Refined copper, essential for conductive wiring and electronics."
-};
