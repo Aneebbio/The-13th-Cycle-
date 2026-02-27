9b5d0890e337b12124ed29c984b260a7afa187a9
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:28:23 +0500

    Create quantum_chip.js

---
+++ resorces.js/quantum_chip.js
@@ -0,0 +1,7 @@
+export const quantum_chip = {
+    id: "quantum_chip",
+    label: "Quantum Chip",
+    type: "component",
+    requires: { item: "advanced_processor", quantity: 1, item2: "diamond", quantity2: 1 },
+    desc: "Uses polished diamonds to manage quantum entanglement data."
+};
