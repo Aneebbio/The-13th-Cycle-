2bf496afb8a60db7bb2c6f49debb7a140cf11b14
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 20:52:36 +0500

    Delete electromagnet.js

--- electromagnet.js
+++
@@ -1,7 +0,0 @@
-export const electromagnet = {
-    id: "electromagnet",
-    label: "Electromagnet",
-    type: "component",
-    requires: { item: "ironingot", quantity: 1, item2: "copper_wire", quantity2: 2 },
-    desc: "A core of iron wrapped in wire. Used in motors and railguns."
-};
