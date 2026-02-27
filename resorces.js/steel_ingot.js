180005fc82a5f76ef97aa374e32d7a8b6fae6315
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:30:13 +0500

    Create steel_ingot.js

---
+++ resorces.js/steel_ingot.js
@@ -0,0 +1,10 @@
+export const steel_ingot = {
+    id: "steel_ingot",
+    label: "Steel Ingot",
+    type: "alloy",
+    requires: { 
+        item1: "iron_ingot", quantity1: 2, 
+        item2: "coal", quantity2: 1 
+    },
+    desc: "An iron-carbon alloy. Much stronger and more durable than pure iron."
+};
