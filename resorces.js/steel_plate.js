3673be4ce7e42785290f0c19851b35ce4bc40078
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:32:28 +0500

    Create steel_plate.js

---
+++ resorces.js/steel_plate.js
@@ -0,0 +1,7 @@
+export const steel_plate = {
+    id: "steel_plate",
+    label: "Reinforced Steel Plate",
+    type: "structural",
+    requires: { item: "steel_ingot", quantity: 2 },
+    desc: "Flat sheets of steel used for wall paneling and armor."
+};
