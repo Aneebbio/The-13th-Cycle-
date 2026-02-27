ad8c4673c9d48f146dcb46c8421a0f30be298822
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 19:03:21 +0500

    Create fluid_pipe_iron.js

---
+++ structures/fluid_pipe_iron.js
@@ -0,0 +1,8 @@
+export const fluid_pipe_iron = {
+    id: "fluid_pipe_iron",
+    label: "Iron Fluid Pipe",
+    type: "logistics",
+    requires: { item: "iron_ingot", quantity: 2 },
+    allows: ["raw_water", "crude_oil", "waste_water"],
+    desc: "A basic pipe for non-corrosive liquids."
+};
