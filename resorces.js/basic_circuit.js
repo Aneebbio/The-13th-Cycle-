593d02b3a184145513e56fd073cd3d886dae3bec
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 19:01:40 +0500

    Create basic_circuit.js

---
+++ resorces.js/basic_circuit.js
@@ -0,0 +1,11 @@
+export const basic_circuit = {
+    id: "basic_circuit",
+    label: "Basic Circuit Board",
+    type: "component",
+    requires: { 
+        item1: "resistor", quantity1: 2, 
+        item2: "capacitor", quantity2: 1,
+        item3: "copper_wire", quantity3: 2 
+    },
+    desc: "A low-tier logic board. Used for simple windmills and storage racks."
+};
