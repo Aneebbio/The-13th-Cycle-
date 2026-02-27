a097780e8855f312d62e9410e172dc374403b6fa
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:32:53 +0500

    Create steel_gear.js

---
+++ resorces.js/steel_gear.js
@@ -0,0 +1,7 @@
+export const steel_gear = {
+    id: "steel_gear",
+    label: "Hardened Steel Gear",
+    type: "mechanical",
+    requires: { item: "steel_ingot", quantity: 1 },
+    desc: "Precision-cut gear that lasts 10x longer than iron gears."
+};
