21814ad4ee2e06692709e78caba3a49d237404f7
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:54:27 +0500

    Create capacitor.js

---
+++ resorces.js/capacitor.js
@@ -0,0 +1,11 @@
+export const capacitor = {
+    id: "capacitor",
+    label: "Industrial Capacitor",
+    type: "component",
+    requires: { 
+        item1: "aluminum_plate", quantity1: 1, 
+        item2: "silica_dust", quantity2: 2,
+        item3: "copper_wire", quantity3: 2
+    },
+    desc: "Stores a small amount of electrical energy. Essential for power stability."
+};
