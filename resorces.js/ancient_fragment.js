c6cfbd486d4a3824eeaa8eb2baeb12954e59dea2
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 20:29:36 +0500

    Create ancient_fragment.js

---
+++ resorces.js/ancient_fragment.js
@@ -0,0 +1,9 @@
+export const ancient_fragment = {
+    id: "ancient_fragment",
+    label: "Ancient Tech Fragment",
+    type: "relic",
+    found_at: "ancient_structure",
+    can_be_refined: true,
+    yields: { item: "quantum_chip", quantity: 1 },
+    desc: "A shard of glass-like metal containing microscopic data circuits."
+};
