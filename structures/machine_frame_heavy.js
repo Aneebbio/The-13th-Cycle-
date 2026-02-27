ba6e0ad11a4298f0c99e702fe5611c0aa0b30efe
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:49:49 +0500

    Create machine_frame_heavy.js

---
+++ structures/machine_frame_heavy.js
@@ -0,0 +1,10 @@
+export const machine_frame_heavy = {
+    id: "machine_frame_heavy",
+    label: "Reinforced Machine Frame",
+    type: "base_component",
+    requires: { 
+        item1: "steel_beam", quantity1: 10, 
+        item2: "concrete_slab", quantity2: 2 
+    },
+    desc: "The vibration-resistant base for heavy industrial extractors."
+};
