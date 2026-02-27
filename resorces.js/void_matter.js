a57cbb21e0b0ffd88eb44208a64a3b04b9a2645b
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:42:58 +0500

    Create void_matter.js

---
+++ resorces.js/void_matter.js
@@ -0,0 +1,7 @@
+export const void_matter = {
+    id: "void_matter",
+    label: "Void Matter",
+    type: "omega_waste",
+    hazard: "existence_failure",
+    desc: "The dark byproduct of the Singularity. It slowly consumes any container it is in."
+};
