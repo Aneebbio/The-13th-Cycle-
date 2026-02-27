a0cd77cbc0f3ad2997989756d1218aa1c5ea5b71
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:49:31 +0500

    Create machine_frame_light.js

---
+++ structures/machine_frame_light.js
@@ -0,0 +1,10 @@
+export const machine_frame_light = {
+    id: "machine_frame_light",
+    label: "Light Machine Frame",
+    type: "base_component",
+    requires: { 
+        item1: "ironingot", quantity1: 5, 
+        item2: "steel_bolt", quantity2: 20 
+    },
+    desc: "The chassis for small Earth-based machines."
+};
