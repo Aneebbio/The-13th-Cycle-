90fc31d7cba9ea8295cb63efedaa2b8c304b98c1
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 20:21:17 +0500

    Create oxygen_tank_mk1.js

---
+++ equipment/oxygen_tank_mk1.js
@@ -0,0 +1,12 @@
+export const oxygen_tank_mk1 = {
+    id: "oxygen_tank_mk1",
+    label: "Basic Oxygen Tank",
+    type: "survival_gear",
+    slot: "back",
+    requires: { 
+        item1: "iron_ingot", quantity1: 5, 
+        item2: "rubber", quantity2: 2 
+    },
+    capacity: 100, // Total oxygen points for exploration
+    desc: "A heavy iron canister. Allows for short trips into the Barrens."
+};
