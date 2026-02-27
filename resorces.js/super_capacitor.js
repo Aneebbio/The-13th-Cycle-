1ab1e2e920cd908b6ce9bca5154c190042b92b28
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:55:41 +0500

    Create super_capacitor.js

---
+++ resorces.js/super_capacitor.js
@@ -0,0 +1,11 @@
+export const super_capacitor = {
+    id: "super_capacitor",
+    label: "Super-Capacitor",
+    type: "component",
+    requires: { 
+        item1: "graphene_sheet", quantity1: 2, 
+        item2: "gold_wire", quantity2: 4,
+        item3: "advanced_processor", quantity3: 1
+    },
+    desc: "An elite component for the most demanding power storage systems."
+};
