2960ae5aa3c41df0448522c5b91ea0ce2f020746
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:58:41 +0500

    Create transistor.js

---
+++ resorces.js/transistor.js
@@ -0,0 +1,7 @@
+export const transistor = {
+    id: "transistor",
+    label: "Silicon Transistor",
+    type: "component",
+    requires: { item: "silicon_wafer", quantity: 1, item2: "gold_wire", quantity2: 1 },
+    desc: "A semiconductor used to switch or amplify signals. The heart of logic gates."
+};
