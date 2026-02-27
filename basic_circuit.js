e6bbe0a92e6635a5652fe16951f531460506f685
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 20:49:57 +0500

    Delete basic_circuit.js

--- basic_circuit.js
+++
@@ -1,7 +0,0 @@
-export const basic_circuit = {
-    id: "basic_circuit",
-    label: "Basic Circuit Board",
-    type: "component",
-    requires: { item: "copper_wire", quantity: 2, item2: "plastic_polymer", quantity2: 1 },
-    desc: "A simple PCB for controlling basic station lights and doors."
-};
