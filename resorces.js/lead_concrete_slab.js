87f19e2a659cba0415a1d00e508006d8f983103c
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:57:58 +0500

    Create lead_concrete_slab.js

---
+++ resorces.js/lead_concrete_slab.js
@@ -0,0 +1,10 @@
+export const lead_concrete_slab = {
+    id: "lead_concrete_slab",
+    label: "Lead-Lined Concrete Slab",
+    type: "structural_product",
+    requires: { 
+        item1: "concrete_slab", quantity1: 1, 
+        item2: "leadingot", quantity2: 2 
+    },
+    desc: "Concrete infused with lead. Essential for protecting the crew from cosmic rays and reactor leaks."
+};
