a6c5a63fe6fc5d3e38539c538a4615b70e3f526a
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:39:17 +0500

    Create concrete_mix.js

---
+++ resorces.js/concrete_mix.js
@@ -0,0 +1,12 @@
+export const concrete_mix = {
+    id: "concrete_mix",
+    label: "Wet Concrete Mix",
+    type: "refined",
+    requires: { 
+        item1: "cement_bag_full", quantity1: 1, 
+        item2: "sand", quantity2: 2,
+        item3: "gravel", quantity3: 2,
+        item4: "purified_water", quantity4: 1
+    },
+    desc: "The wet mixture before it hardens into slabs."
+};
