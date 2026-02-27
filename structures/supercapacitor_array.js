993356dd04b3b08474c4bfaf849d3e821d82f9a8
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:53:00 +0500

    Create supercapacitor_array.js

---
+++ structures/supercapacitor_array.js
@@ -0,0 +1,13 @@
+export const supercapacitor_array = {
+    id: "supercapacitor_array",
+    label: "Supercapacitor Array",
+    type: "storage",
+    requires: { 
+        item1: "quantum_chip", quantity1: 1, 
+        item2: "gold_wire", quantity2: 20,
+        item3: "advanced_processor", quantity3: 2
+    },
+    storage_capacity: 100000,
+    discharge_rate: "ultra-fast",
+    desc: "Capable of dumping massive amounts of energy instantly to power the Infinity Core."
+};
