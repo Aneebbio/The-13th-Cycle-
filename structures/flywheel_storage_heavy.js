3e7241a4c64de8d2296bde30d2f57b14dbe2b8d9
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:52:29 +0500

    Create flywheel_storage_heavy.js

---
+++ structures/flywheel_storage_heavy.js
@@ -0,0 +1,13 @@
+export const flywheel_storage_heavy = {
+    id: "flywheel_storage_heavy",
+    label: "Heavy Flywheel Storage",
+    type: "storage",
+    requires: { 
+        item1: "steel_gear", quantity1: 10, 
+        item2: "electromagnet", quantity2: 4,
+        item3: "concrete_slab", quantity3: 2 
+    },
+    storage_capacity: 15000,
+    efficiency: "90%",
+    desc: "A high-speed spinning cylinder that stores energy kinetically. No chemical wear."
+};
