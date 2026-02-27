c5609b5d892b9545dfdb1cebc3e3501692edab71
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:55:21 +0500

    Create graphene_sheet.js

---
+++ resorces.js/graphene_sheet.js
@@ -0,0 +1,7 @@
+export const graphene_sheet = {
+    id: "graphene_sheet",
+    label: "Graphene Sheet",
+    type: "refined_carbon",
+    requires: { item: "coal", quantity: 10 }, // Heavy refining of carbon
+    desc: "A single layer of carbon atoms. Highly conductive and incredibly strong."
+};
