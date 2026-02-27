b9368eaf96ad129ae3db8133dba10a8baa78bdca
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:58:23 +0500

    Create resistor.js

---
+++ resorces.js/resistor.js
@@ -0,0 +1,7 @@
+export const resistor = {
+    id: "resistor",
+    label: "Carbon Resistor",
+    type: "component",
+    requires: { item: "coal", quantity: 1, item2: "copper_wire", quantity2: 1 },
+    desc: "A small component that limits electrical flow. Made from carbon (coal)."
+};
