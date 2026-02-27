ba3ad1832e7b7d8fa8881e9fff90c428e37eaf76
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:28:00 +0500

    Create advanced_processor.js

---
+++ resorces.js/advanced_processor.js
@@ -0,0 +1,7 @@
+export const advanced_processor = {
+    id: "advanced_processor",
+    label: "Advanced Processor",
+    type: "component",
+    requires: { item: "silicon_wafer", quantity: 2, item2: "gold_wire", quantity2: 1 },
+    desc: "A high-speed CPU required for automated mining and research."
+};
