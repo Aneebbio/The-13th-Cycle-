5bc1b27dbdbc208d4752b25424138476803f3aaf
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:33:38 +0500

    Create steel_drill_bit.js

---
+++ resorces.js/steel_drill_bit.js
@@ -0,0 +1,7 @@
+export const steel_drill_bit = {
+    id: "steel_drill_bit",
+    label: "Steel Drill Bit",
+    type: "tool_part",
+    requires: { item: "steel_ingot", quantity: 1 },
+    desc: "Replacement bit for mining drills. Increases mining speed."
+};
