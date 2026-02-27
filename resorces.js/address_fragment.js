f11669e42953196fd4ab5d9ba0010c4dd72040ee
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 20:35:24 +0500

    Create address_fragment.js

---
+++ resorces.js/address_fragment.js
@@ -0,0 +1,7 @@
+export const address_fragment = {
+    id: "address_fragment",
+    label: "Encoded Glyph Fragment",
+    type: "relic",
+    total_required: 3,
+    desc: "A piece of a coordinate string. It looks like a sequence of shifting stars."
+};
