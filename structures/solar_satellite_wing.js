6dc6c88f494ab7b3e3b3bdd9aa023eaf04b90fa9
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:53:06 +0500

    Create solar_satellite_wing.js

---
+++ structures/solar_satellite_wing.js
@@ -0,0 +1,12 @@
+export const solar_satellite_wing = {
+    id: "solar_satellite_wing",
+    label: "Satellite Solar Wing",
+    type: "space_generator",
+    requires: { 
+        item1: "gold_wire", quantity1: 10, 
+        item2: "silicon_wafer", quantity2: 8,
+        item3: "aluminum_plate", quantity3: 4
+    },
+    output: { power: 300 },
+    desc: "Ultra-lightweight gold-threaded panels designed for vacuum environments."
+};
