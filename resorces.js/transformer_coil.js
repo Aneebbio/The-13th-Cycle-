9596af65a500849ee1885291a0307a27493573ed
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 18:55:00 +0500

    Create transformer_coil.js

---
+++ resorces.js/transformer_coil.js
@@ -0,0 +1,10 @@
+export const transformer_coil = {
+    id: "transformer_coil",
+    label: "Transformer Coil",
+    type: "component",
+    requires: { 
+        item1: "electromagnet", quantity1: 1, 
+        item2: "copper_wire", quantity3: 10 
+    },
+    desc: "Used to step-up or step-down voltage in power grids."
+};
