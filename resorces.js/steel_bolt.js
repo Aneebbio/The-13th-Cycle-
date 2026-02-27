a23299bd5ea69e637a1d0f451c5f1d32b6535e93
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:33:59 +0500

    Create steel_bolt.js

---
+++ resorces.js/steel_bolt.js
@@ -0,0 +1,7 @@
+export const steel_bolt = {
+    id: "steel_bolt",
+    label: "Steel Bolts (Box)",
+    type: "hardware",
+    requires: { item: "steel_ingot", quantity: 1 },
+    desc: "Standard fasteners used to assemble machine modules."
+};
