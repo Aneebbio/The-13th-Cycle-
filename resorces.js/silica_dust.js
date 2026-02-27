173a4e1bc2ee5aa10b0f43a162d44b2f55bdd79f
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:38:31 +0500

    Create silica_dust.js

---
+++ resorces.js/silica_dust.js
@@ -0,0 +1,6 @@
+export const silica_dust = {
+    id: "silica_dust",
+    label: "Silica Dust",
+    type: "byproduct",
+    desc: "Fine powder filtered from sand. Used in advanced electronics."
+};
