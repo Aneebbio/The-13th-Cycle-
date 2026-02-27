f102b02509f6d663d283e4c5956a5cce24fb996d
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:35:57 +0500

    Create cement_bag_full.js

---
+++ resorces.js/cement_bag_full.js
@@ -0,0 +1,10 @@
+export const cement_bag_full = {
+    id: "cement_bag_full",
+    label: "Bag of Cement",
+    type: "material",
+    requires: { 
+        item1: "empty_cement_bag", quantity1: 1, 
+        item2: "limestone", quantity2: 3 
+    },
+    desc: "Dry cement powder. Just add water and sand to create concrete."
+};
