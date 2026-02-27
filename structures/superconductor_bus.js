7eae36b68a6a12d7e7f4dc88d36a367f0dbeb159
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 19:02:59 +0500

    Create superconductor_bus.js

---
+++ structures/superconductor_bus.js
@@ -0,0 +1,8 @@
+export const superconductor_bus = {
+    id: "superconductor_bus",
+    label: "Superconductor Bus Bar",
+    type: "connection_advanced",
+    requires: { item: "gold_wire", quantity: 5, item2: "liquid_nitrogen", quantity2: 10 },
+    transfer_rate: 999999, 
+    desc: "A zero-resistance power line. Required for the Fusion Reactor and the 13th Gate."
+};
