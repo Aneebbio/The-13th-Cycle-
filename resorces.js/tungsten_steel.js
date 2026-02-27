9cae89ca76ba35c750e38ac92f681be106902a33
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:30:54 +0500

    Create tungsten_steel.js

---
+++ resorces.js/tungsten_steel.js
@@ -0,0 +1,10 @@
+export const tungsten_steel = {
+    id: "tungsten_steel",
+    label: "Tungsten Steel",
+    type: "alloy",
+    requires: { 
+        item1: "steel_ingot", quantity1: 1, 
+        item2: "tungsten_ingot", quantity2: 1 
+    },
+    desc: "Extreme-density alloy used for drill bits and heavy plating."
+};
