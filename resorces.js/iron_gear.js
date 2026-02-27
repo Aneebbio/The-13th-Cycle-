91b15b1e755f9fa07433472cd51111d4c2aba8a3
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:28:46 +0500

    Create iron_gear.js

---
+++ resorces.js/iron_gear.js
@@ -0,0 +1,7 @@
+export const iron_gear = {
+    id: "iron_gear",
+    label: "Iron Gear",
+    type: "component",
+    requires: { item: "ironingot", quantity: 1 },
+    desc: "A simple toothed wheel used for mechanical torque."
+};
