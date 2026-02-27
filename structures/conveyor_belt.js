6db7d2bb681c27d5c2d19763136b13b15d141795
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 19:04:23 +0500

    Create conveyor_belt.js

---
+++ structures/conveyor_belt.js
@@ -0,0 +1,8 @@
+export const conveyor_belt = {
+    id: "conveyor_belt",
+    label: "Conveyor Belt",
+    type: "logistics_solid",
+    requires: { item: "iron_gear", quantity: 1, item2: "rubber", quantity2: 3 },
+    speed: 10, // Items per minute
+    desc: "A motorized belt for moving solid resources between chests and machines."
+};
