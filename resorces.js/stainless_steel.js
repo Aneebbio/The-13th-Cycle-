444f74287bb4b2dfd883d3a86e0695c109776be1
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:30:34 +0500

    Create stainless_steel.js

---
+++ resorces.js/stainless_steel.js
@@ -0,0 +1,11 @@
+export const stainless_steel = {
+    id: "stainless_steel",
+    label: "Stainless Steel",
+    type: "alloy",
+    requires: { 
+        item1: "steel_ingot", quantity1: 1, 
+        item2: "nickel_ingot", quantity2: 1,
+        item3: "chromium_ingot", quantity3: 1
+    },
+    desc: "Corrosion-resistant steel for medical tools and reactor parts."
+};
