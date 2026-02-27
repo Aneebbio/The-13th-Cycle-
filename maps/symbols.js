ea77a2ede7158dbc9eb47dde1be05a99aa3d5b2c
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 20:09:57 +0500

    Create symbols.js

---
+++ maps/symbols.js
@@ -0,0 +1,12 @@
+// A simple procedural icon for a Power Structure
+export const icon_windmill = `
+<svg width="40" height="40" viewBox="0 0 40 40">
+  <circle cx="20" cy="20" r="2" fill="white" />
+  <path d="M20 20 L20 5 M20 20 L35 25 M20 20 L5 25" stroke="cyan" stroke-width="2" />
+</svg>`;
+
+// A simple procedural icon for a Mine/Quarry
+export const icon_mine = `
+<svg width="40" height="40" viewBox="0 0 40 40">
+  <path d="M10 30 L20 10 L30 30 Z" fill="none" stroke="yellow" stroke-width="2" />
+</svg>`;
