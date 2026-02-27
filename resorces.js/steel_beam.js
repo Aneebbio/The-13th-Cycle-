44deef48cadf49adead17d7502ad8aaf6fa0bae6
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:32:09 +0500

    Create steel_beam.js

---
+++ resorces.js/steel_beam.js
@@ -0,0 +1,7 @@
+export const steel_beam = {
+    id: "steel_beam",
+    label: "Steel I-Beam",
+    type: "structural",
+    requires: { item: "steel_ingot", quantity: 4 },
+    desc: "A heavy-duty support beam used for station hulls and large floors."
+};
