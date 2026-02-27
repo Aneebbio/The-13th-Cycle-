acddebae8213523d33d61988b75ba8b84d2fe3e0
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:38:07 +0500

    Create fertilized_soil.js

---
+++ resorces.js/fertilized_soil.js
@@ -0,0 +1,10 @@
+export const fertilized_soil = {
+    id: "fertilized_soil",
+    label: "Fertilized Soil",
+    type: "processed",
+    requires: { 
+        item1: "dirt", quantity1: 2, 
+        item2: "fertilizer_slurry", quantity2: 1 
+    },
+    desc: "Enriched dirt capable of growing trees and food crops in space."
+};
