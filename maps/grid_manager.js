1bbf7f81ed6043633c572fb1bf3b4f53c8edbcde
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 20:11:19 +0500

    Create grid_manager.js

---
+++ maps/grid_manager.js
@@ -0,0 +1,9 @@
+export const map_grid = {
+    grid_size: 50, // Each square is 50x50 pixels
+    background_color: "#0a0a0f", // Dark space/radar color
+    line_color: "#1f1f2e",
+    locations: [
+        { id: "iron_barrens", x: 2, y: 3, icon: "icon_mine" },
+        { id: "wind_farm", x: 5, y: 1, icon: "icon_windmill" }
+    ]
+};
