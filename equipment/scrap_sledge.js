0dfdf8ec399f7477f44770c724c13549a464f391
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 20:21:42 +0500

    Create scrap_sledge.js

---
+++ equipment/scrap_sledge.js
@@ -0,0 +1,13 @@
+export const scrap_sledge = {
+    id: "scrap_sledge",
+    label: "Scrap Sledgehammer",
+    type: "weapon",
+    slot: "hand",
+    requires: { 
+        item1: "steel_beam", quantity1: 1, 
+        item2: "rubber", quantity2: 3 
+    },
+    damage: 15,
+    durability: 100,
+    desc: "A heavy piece of industrial debris welded to a pipe. Good for smashing drones."
+};
