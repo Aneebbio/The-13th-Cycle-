6558cce6e5f044815195cd61f400b12a7fe5b719
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:33:14 +0500

    Create steel_cable.js

---
+++ resorces.js/steel_cable.js
@@ -0,0 +1,7 @@
+export const steel_cable = {
+    id: "steel_cable",
+    label: "Steel Cable",
+    type: "mechanical",
+    requires: { item: "steel_ingot", quantity: 1 },
+    desc: "High-tension braided wire for elevators and crane systems."
+};
