579e0023fd2418800dbb85ec58faea69688464dd
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:35:09 +0500

    Create empty_cement_bag.js

---
+++ resorces.js/empty_cement_bag.js
@@ -0,0 +1,7 @@
+export const empty_cement_bag = {
+    id: "empty_cement_bag",
+    label: "Empty Cement Bag",
+    type: "packaging",
+    requires: { item: "wood", quantity: 1 }, // Representing heavy paper/fiber
+    desc: "A heavy-duty reinforced paper bag for transporting dry minerals."
+};
