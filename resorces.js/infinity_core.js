992d59129d630b2a93555e8b897cf7d1d1b4a4d9
Aneebbio <aneebmuktiar239@gmail.com>
2026-02-27 16:42:28 +0500

    Create infinity_core.js

---
+++ resorces.js/infinity_core.js
@@ -0,0 +1,11 @@
+export const infinity_core = {
+    id: "infinity_core",
+    label: "Infinity Core",
+    type: "end_game_engine",
+    requires: { 
+        item1: "everythingness", quantity1: 1, 
+        item2: "liquid_hydrogen", quantity2: 500,
+        item3: "stainless_steel", quantity3: 50
+    },
+    desc: "A perpetual motion device that powers the station across dimensions."
+};
